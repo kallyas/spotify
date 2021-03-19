1ee34fdb2da2158190b9f7c3f34a4da2a9a6e74b
Kally <kallyasmedia@gmail.com>
2021-03-19 09:38:24 +0000

    add server, dashboard

---
+++ client/src/Login.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+require("dotenv").config();
+
+const CLIENT_ID = `854502f7601d4411a26e99589d81726a`;
+
+const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=code
+&redirect_uri=https://3000-violet-wren-dipgm51t.ws-eu03.gitpod.io&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state`;
+
+export default function Login() {
+  return (
+    <Container
+      className="d-flex justify-content-center
+        align-items-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <a className="btn btn-success btn-lg" href={AUTH_URL}>
+        Login With spotify
+      </a>
+    </Container>
+  );
+}
